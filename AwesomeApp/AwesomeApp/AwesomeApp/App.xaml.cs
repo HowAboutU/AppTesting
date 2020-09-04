79a36ceefc5cd24d4634a1a108e7c623e69280a6
gina <geena133@gmail.com>
2020-09-04 15:19:35 +0800

    first commit

---
+++ AwesomeApp/AwesomeApp/AwesomeApp/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AwesomeApp.Services;
+using AwesomeApp.Views;
+
+namespace AwesomeApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            //MainPage = new AppShell();
+            MainPage = new Greetings();
+
+            //MainPage = new ContentPage
+            //{
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+
+            //        Children =
+            //        {
+            //            new Label
+            //            {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AwesomeApp/AwesomeApp/AwesomeApp/Greetings.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Greetings : ContentPage
+    {
+        public Greetings()
+        {
+            InitializeComponent();
+
+            Content = new Label
+            {
+                Text = "\u201CGreetings, Xamarin.Forms",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.FromRgb(1, 0, 0),
+                TextColor = Color.FromRgb(1.0, 0, 0),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+
+            Padding = new Thickness(5, Device.OnPlatform(20, 5, 5 ), 5, 5);
+        }
+    }
+}
