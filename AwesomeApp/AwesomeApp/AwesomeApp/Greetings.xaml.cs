c3287f91a50ecb12d225f42b0c9fcba1c0b3424d
gina <geena133@gmail.com>
2020-09-04 16:25:15 +0800

    add

--- AwesomeApp/AwesomeApp/AwesomeApp/Greetings.xaml.cs
+++ AwesomeApp/AwesomeApp/AwesomeApp/Greetings.xaml.cs
@@ -16,18 +16,35 @@ namespace AwesomeApp
         {
             InitializeComponent();
 
+            FormattedString formattedString = new FormattableString();
+            formattedString.Spans.Add(new Span
+            {
+                Text = "I "
+            });
+
+            formattedString.Spans.Add(new Span {
+                Text ="love",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            });
+
+            formattedString.Spans.Add(new Span {
+                Text = " Xamarin.Forms!"
+            });
+
             Content = new Label
             {
-                Text = "\u201CGreetings, Xamarin.Forms",
+                //Text = "\u201CGreetings, Xamarin.Forms",
+                FormattedText = formattedString,
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.Center,
-                BackgroundColor = Color.FromRgb(1, 0, 0),
-                TextColor = Color.FromRgb(1.0, 0, 0),
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+                //BackgroundColor = Color.FromRgb(1, 0, 0),
+                //TextColor = Color.FromRgb(1.0, 0, 0),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+                //FontAttributes = FontAttributes.Bold | FontAttributes.Italic
             };
 
-            Padding = new Thickness(5, Device.OnPlatform(20, 5, 5 ), 5, 5);
+            //Padding = new Thickness(5, Device.OnPlatform(20, 5, 5 ), 5, 5);
         }
     }
 }
